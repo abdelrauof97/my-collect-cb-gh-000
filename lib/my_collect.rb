1441e6adb2a1395dfae04730ec5499d7ca066082
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-09 19:27:53 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,12 @@
 
 def collect(array)
   i = 0
+  new_array = []
   while i < array.length
-    yield(array)
+    new_array << yield(array[i])
     i += 1 
   end
+  new_array
 end
 students = ["Tim Jones", "Tom Smith", "Jim Campagno"]
 collect(students) { |x|
